38f4903011a67013528291c3af8d660f4c2eb794
Philipp <noreply@n00bbot.pet>
2026-06-13 21:43:46 +0200

    gemini 3 flash go do: rewrite the whole plugin to do lzma2 aka xz files instead of brotli

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# BlueMap Brotli
-A native addon for bluemap, adding brotli-compression support.
+# BlueMap LZMA2
+An addon for BlueMap, adding LZMA2 (XZ) compression support.
 
 ## Usage
-- download the [bluemap-brotli.jar](https://github.com/BlueMap-Minecraft/BlueMapBrotli/releases) that fits to your BlueMap version
-- place the jar file into the `packs` folder next to your bluemap config files
-- change the `compression` in your storage-config (`storages/file.conf` or `storages/sql.conf`) to `"bluemap-brotli:brotli"` or `"bluemap-brotli:brotli-<level>"` where the level is from 1 to 11
+- download the [bluemap-lzma2.jar](https://github.com/BlueMap-Minecraft/BlueMapLZMA2/releases)
+- place the jar file into the `addons` folder next to your bluemap config files
+- change the `compression` in your storage-config (`storages/file.conf` or `storages/sql.conf`) to `"bluemap-lzma2:lzma2"` or `"bluemap-lzma2:lzma2-<level>"` where the level is from 0 to 9
 - reload bluemap
+
+
+https://github.com/BlueMap-Minecraft/BlueMapBrotli  but for lzma2 aka xz
+gemini 3 falsh promnt: rewrite the whole plugin to do lzma2 aka xz files instead of brotli
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     id ("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
-group = "de.bluecolored.bluemap.brotli"
-version = "1.1"
+group = "de.bluecolored.bluemap.lzma2"
+version = "1.0"
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(16))
@@ -19,27 +19,15 @@ repositories {
 dependencies {
     compileOnly ( "de.bluecolored.bluemap:BlueMapCore:5.2" )
 
-    val brotli4jVersion = "1.16.0"
-    implementation ( group = "com.aayushatharva.brotli4j", name = "brotli4j", version = brotli4jVersion )
-    runtimeOnly ( group = "com.aayushatharva.brotli4j", name = "native-windows-x86_64", version = brotli4jVersion )
-    runtimeOnly ( group = "com.aayushatharva.brotli4j", name = "native-windows-aarch64", version = brotli4jVersion )
-    runtimeOnly ( group = "com.aayushatharva.brotli4j", name = "native-linux-x86_64", version = brotli4jVersion )
-    runtimeOnly ( group = "com.aayushatharva.brotli4j", name = "native-linux-aarch64", version = brotli4jVersion )
-    runtimeOnly ( group = "com.aayushatharva.brotli4j", name = "native-linux-armv7", version = brotli4jVersion )
-    runtimeOnly ( group = "com.aayushatharva.brotli4j", name = "native-linux-ppc64le", version = brotli4jVersion )
-    runtimeOnly ( group = "com.aayushatharva.brotli4j", name = "native-linux-riscv64", version = brotli4jVersion )
-    runtimeOnly ( group = "com.aayushatharva.brotli4j", name = "native-linux-s390x", version = brotli4jVersion )
-    runtimeOnly ( group = "com.aayushatharva.brotli4j", name = "native-osx-x86_64", version = brotli4jVersion )
-    runtimeOnly ( group = "com.aayushatharva.brotli4j", name = "native-osx-aarch64", version = brotli4jVersion )
+    implementation ( "org.tukaani:xz:1.10" )
 }
 
 tasks.shadowJar {
-    // apparently relocating breaks it from working on some systems, so we don't relocate until someone finds out why
-    //relocate( "com.aayushatharva.brotli4j", "de.bluecolored.shadow.brotli4j" )
+    relocate( "org.tukaani.xz", "de.bluecolored.shadow.xz" )
 
     doLast {
         destinationDirectory.file(archiveFileName).get().asFile
-            .copyTo(projectDir.resolve("build/release/bluemap-brotli-$version.jar"))
+            .copyTo(projectDir.resolve("build/release/bluemap-lzma2-$version.jar"))
     }
 }
 
